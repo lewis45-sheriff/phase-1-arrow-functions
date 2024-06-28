4291df293ea5d774d5eb98f5e5a623c396052eba
lewis <lewiskipkemoi53@gmail.com>
2024-06-28 19:31:55 +0300

    all

---
+++ index.js
@@ -0,0 +1,13 @@
+const divide =function() {
+    return 2000/100;
+
+};
+const square=(x)=>{
+    return x*x;
+};
+const add=(a,b)=>{
+    return a+b;
+};
+console.log(divide());
+console.log(5);
+console.log(3,5);
